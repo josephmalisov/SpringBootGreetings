ffa85810700710693c8f61faec4a9629e577d3ae
Joseph Malisov <jam821@lehigh.edu>
2019-12-28 01:44:00 -0500

    changed from RestController to Controller, which allows view to be from HTML templates

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,29 +1,28 @@
 package hello;
 
-import java.util.ArrayList;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class GreetingController {
 
   private static final String template = "Hello, %s!";
   private final AtomicLong counter = new AtomicLong();
 
   @RequestMapping("/greetings")
-  public ArrayList<Greeting> greeting(Model model) {
+  public String greeting(Model model) {
     model.addAttribute("greetings", Application.arrGreetings);
-    return Application.arrGreetings;
+    return "greetings";
   }
 
   @RequestMapping("/greetings/add")
-  public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+  public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name, Model model) {
     Greeting newGreeting = new Greeting(counter.incrementAndGet(), String.format(template, name)); //create the new Greeting object
     Application.arrGreetings.add(newGreeting); //add the new Greeting to the list
-
+    model.addAttribute("name", name); //used to display the name in the HTML
     return newGreeting;
   }
   
