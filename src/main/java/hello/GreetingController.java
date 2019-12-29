e99973e6ff98be1fccdc8780be40430b4f098fbf
Joseph Malisov <jam821@lehigh.edu>
2019-12-29 14:50:56 -0500

    working on submission form for adding greetings

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -3,8 +3,11 @@ package hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
@@ -12,18 +15,18 @@ public class GreetingController {
   private static final String template = "Hello, %s!";
   private final AtomicLong counter = new AtomicLong();
 
-  @RequestMapping("/greetings")
+  @GetMapping("/greetings")
   public String greeting(Model model) {
     model.addAttribute("greetings", Application.arrGreetings);
     return "greetings";
   }
 
-  @RequestMapping("/greetings/add")
-  public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name, Model model) {
-    Greeting newGreeting = new Greeting(counter.incrementAndGet(), String.format(template, name)); //create the new Greeting object
-    Application.arrGreetings.add(newGreeting); //add the new Greeting to the list
-    model.addAttribute("name", name); //used to display the name in the HTML
-    return newGreeting;
+  @PostMapping("/greetings")
+  public String greetingSubmit(@ModelAttribute Greeting greeting) {
+    //Greeting newGreeting = new Greeting(counter.incrementAndGet(), String.format(template, name)); //create the new Greeting object
+    Application.arrGreetings.add(greeting); //add the new Greeting to the list
+    //model.addAttribute("name", name); //used to display the name in the HTML
+    return "result";
   }
   
 }
